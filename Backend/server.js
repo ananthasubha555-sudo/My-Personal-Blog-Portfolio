bdb25a76eee244671f45af6dd1fa481efcb5918c
ananthasubha555-sudo <ananthasubha555@gmail.com>
2026-01-11 10:36:01 +0530

    Refactor CORS middleware configuration

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST",
+  })
+);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/api/contact", contactRoutes);
 
